8003bcf866b370af7f886a0e9e5be4a18cbf4548
xz <xzjayx@126.com>
2018-10-31 16:16:48 +0800

    测试

---
+++ src/main/java/com/test/Test.java
@@ -0,0 +1,14 @@
+package com.test;
+
+/**
+ * @Author: xz
+ * @Description:
+ * @Date: Create in 16:16 2018/10/31/031
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("111");
+    }
+
+}
